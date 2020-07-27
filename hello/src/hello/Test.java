d770bc47380ff97d112f0d3f56158acdb17afba8
shizesen1 <2313514353@qq.com>
2020-07-27 14:52:14 +0800

    新增

---
+++ hello/src/hello/Test.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		String a="aaa";
+		System.out.println("hello");
+	}
+}
---
+++ hello/src/hello/rr.java
@@ -0,0 +1,16 @@
+package hello;
+/**@文件名: rr.java
+ * @类功能说明: 
+ * @作者: ShiZeSen
+ * @Email: 2313514353@qq.com
+ * @日期: 2020年7月1日下午6:19:50
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: ShiZeSen</li> 
+ * 	 <li>日期: 2020年7月1日下午6:19:50</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class rr {
+
+}
